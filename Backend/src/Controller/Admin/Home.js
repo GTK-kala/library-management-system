a654a008efb7e79fa5d9db187a4f0bc7ba80d9c8
GTK-kala <gtkkala1110@gmail.com>
2025-12-18 14:26:29 +0300

    Fetching demo data from the database

--- Backend/src/Controller/Admin/Home.js
+++ Backend/src/Controller/Admin/Home.js
@@ -1,5 +1,17 @@
+import connection from "../../Config/db.js";
+
 export const HomeRoute = (req, res) => {
-  return res.status(200).json({
-    message: "Server is running on /home route",
+  const sql = "SELECT * FROM users";
+
+  connection.query(sql, (err, result) => {
+    if (err) {
+      return res.status(500).json({
+        message: "Error on connection to db",
+      });
+    }
+    return res.status(200).json({
+      message: "Data Fetched ok",
+      result: result,
+    });
   });
 };

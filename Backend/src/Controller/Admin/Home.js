5fb276457eb00112ce5316311e8a7990aca8d92b
GTK-kala <gtkkala1110@gmail.com>
2025-12-18 14:31:26 +0300

    Error handling

--- Backend/src/Controller/Admin/Home.js
+++ Backend/src/Controller/Admin/Home.js
@@ -1,17 +1,23 @@
 import connection from "../../Config/db.js";
 
 export const HomeRoute = (req, res) => {
-  const sql = "SELECT * FROM users";
+  try {
+    const sql = "SELECT * FROM users";
 
-  connection.query(sql, (err, result) => {
-    if (err) {
-      return res.status(500).json({
-        message: "Error on connection to db",
+    connection.query(sql, (err, result) => {
+      if (err) {
+        return res.status(500).json({
+          message: "Error on database",
+        });
+      }
+      return res.status(200).json({
+        message: "Data Fetched ok",
+        result: result,
       });
-    }
-    return res.status(200).json({
-      message: "Data Fetched ok",
-      result: result,
     });
-  });
+  } catch (error) {
+    res.status(404).json({
+      message: "Error on the connection",
+    });
+  }
 };

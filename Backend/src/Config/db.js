af3d06057aadb66df0cd2a390cea8a9e2c9e773b
GTK-kala <gtkkala1110@gmail.com>
2025-12-18 17:06:19 +0300

    continued on previous work

--- Backend/src/Config/db.js
+++ Backend/src/Config/db.js
@@ -3,9 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const Url_Db = `mysql://${process.env.MYSQL_USER}:${process.env.MYSQL_PASSWORD}@${process.env.MYSQL_HOST}:${process.env.MYSQL_PORT}/${process.env.MYSQL_DATABASE}`;
-
-const connection = mysql.createConnection(Url_Db);
+const connection = mysql.createConnection(process.env.DATABASE_URL);
 
 connection.connect(() => {
   console.log("Database Connected !!!");
